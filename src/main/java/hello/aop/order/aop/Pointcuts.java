44df8fbfa7f8f1432a3f271a01f65b69594be41d
윤설이 <86522955+Comesfullcircle@users.noreply.github.com>
2024-02-22 19:35:33 +0900

    스프링 AOP 구현5 - 어드바이스 순서

--- src/main/java/hello/aop/order/aop/Pointcuts.java
+++ src/main/java/hello/aop/order/aop/Pointcuts.java
@@ -6,13 +6,14 @@ public class Pointcuts {
 
     //hello.aop.order 패키지와 하위 패키지
     @Pointcut("execution(* hello.aop.order..*(..))")
-    private void allOrder() {} //pointcut signature
+    public void allOrder(){} //pointcut signature
 
     //클래스 이름 패턴이 *Service
     @Pointcut("execution(* *..*Service.*(..))")
-    private void allService(){}
+    public void allService(){}
 
     //allOrder && allService
     @Pointcut("allOrder() && allService()")
     public void orderAndService() {}
+
 }
